8c416356b38c4d42a7c444a4013e498fe73e6d78
gitTrialEra <121790031+gitTrialEra@users.noreply.github.com>
2023-01-02 14:20:28 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,9 @@
 class Sum{
     public static void main (String args[]) {
         int a = 3;
-        int b = 8;
+        int b = 11;
+        int c = 25;
         System.out.println(a+b);
+        System.out.println(a+c);
     }
 }
